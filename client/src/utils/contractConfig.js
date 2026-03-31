8d9098aab28b5f5a0cf32bd8fd015cf11e5c1338
kaghazi12 <gulraiz1270@gmail.com>
2026-04-01 03:17:48 +0500

    v2.0111

--- client/src/utils/contractConfig.js
+++ client/src/utils/contractConfig.js
@@ -1,4 +1,4 @@
-﻿export const CONTRACT_ADDRESS = "0xBf5b212d8879Fbf97A15bE94683fa62eAcB099BF";
+export const CONTRACT_ADDRESS = "0xBf5b212d8879Fbf97A15bE94683fa62eAcB099BF";
 
 // Load elections metadata (created during init.js)
 let metadataPromise = fetch('/electionsMetadata.json')
